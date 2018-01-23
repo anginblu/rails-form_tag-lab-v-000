220c2082421dbdbcb9d4e952ca70dc1d2fd4fe8b
Jacqueline Lee <anginblu@gmail.com>
2018-01-23 20:36:42 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,14 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
   def show
